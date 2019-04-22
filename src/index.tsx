91b08758b2acf5db6c0802d96cf66c5bd1f422ee
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2019-04-22 20:42:00 -0300

    added sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import * as React from "react";
 import { render } from "react-dom";
 import Index from "./pages";
 
+import "./styles/styles.scss";
+
 render(<Index />, document.getElementById("root"));
